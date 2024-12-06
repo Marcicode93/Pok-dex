2849429be7f57f037bca12c72c1abd81c2af7059
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2024-12-07 00:27:06 +0100

    latest version

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let pokemonArr = [];
 let currentIndex = 0;
+let offset = 0;
 
-async function fetchItem() {
+async function fetchItem(limit = 30) {
+  if (offset >= 151) {
+    console.log("Alle Pokemon geladen");
+    return;
+  }
   let response = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=30&offset=0"
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   );
   let responseAsJson = await response.json();
   let results = responseAsJson.results;
@@ -11,9 +16,23 @@ async function fetchItem() {
   for (let i = 0; i < results.length; i++) {
     let pokemonResponse = await fetch(results[i].url);
     let pokemonData = await pokemonResponse.json();
-    pokemonArr.push(pokemonData);
-    renderCharacter(pokemonData, i);
+
+    if (!pokemonArr.find((p) => p.name === pokemonData.name)) {
+      pokemonArr.push(pokemonData); // Nur hinzufügen, wenn es noch nicht existiert
+      renderCharacter(pokemonData, pokemonArr.length - 1);
+    }
   }
+
+  offset += limit;
+  if (offset >= 151) {
+    let loadButton = document.getElementById("load-more-btn");
+      loadButton.classList.add("display-none");
+      loadButton.classList.remove('load');
+  }
+}
+
+function fetchMorePokemon() {
+  fetchItem();
 }
 
 function renderCharacter(pokemon, i) {
@@ -30,10 +49,10 @@ function renderCharacter(pokemon, i) {
   }
 
   content.innerHTML += /*html*/ `
-    <div class="card" style="width: 18rem;" onclick="toggleOverlay(event,${i})">
-      <img class="card-img-top" src="${imageUrl}" alt="image-Pokémon">
+    <div class="card card-hover" onclick="toggleOverlay(event,${i})">
+      <img class="card-img-top" id="main-page-img" src="${imageUrl}" alt="image-Pokémon">
       <div class="card-body">
-        <h3 class="card-title avatar-name">${name}</h3>
+        <h5 class="card-title avatar-name">${name}</h5>
         <div class="types">${typesHTML}</div>
         </div>
     </div>
@@ -41,12 +60,20 @@ function renderCharacter(pokemon, i) {
 }
 
 function renderPokemoninOverlay(pokemon) {
-  //   if (!pokemon) {
-  //     console.error("Pokemon is undefined in renderPokemoninOverlay.");
-  //     return;
-  // }
   let overlayContent = document.getElementById("overlay-content");
   let name = pokemon.name;
+  let height = pokemon.height * 10;
+  let heightDisplay =
+    height >= 100
+      ? (height / 100).toFixed(2).toString().replace(".", ",") + " m"
+      : height + " cm";
+  let weight = pokemon.weight;
+  let weightDisplay = weight >= 100 ? weight / 10 + " kg" : weight + " g";
+
+  let abilitiesHTML = "";
+  pokemon.abilities.forEach((ability) => {
+    abilitiesHTML += `<li>${ability.ability.name}</li>`;
+  });
 
   let statsHTML = "";
   for (let k = 0; k < pokemon.stats.length; k++) {
@@ -84,28 +111,44 @@ function renderPokemoninOverlay(pokemon) {
     }
   }
 
+  let mainTypeClass = pokemon.types[0].type.name;
+
   overlayContent.innerHTML = /*html*/ `
-    <div class="card overlay-card">
-  <img class="card-img-top" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Card image cap">
-  <div class="card-body">
-    <h3 class="card-title">${name}</h3>
-    <p class="card-text">Types: ${typesHTML}</p>
-  </div>
-  <nav>
-  <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">About</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Stats</a>
-    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Moves</a>
-  </div>
-</nav>
-<div class="tab-content" id="nav-tabContent">
-  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
-</div>
-  <ul class="list-group list-group-flush">
- ${statsHTML}
-  </ul>
+  <div class="card overlay-card">
+      <div class="upper-card ${mainTypeClass}">
+        <img class="card-img-top" id="img-card" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Card image cap">
+         <div class="card-body">
+          <h3 class="card-title">${name}</h3>
+          <p class="card-text">Types: ${typesHTML}</p>
+        </div>
+      </div>
+
+      <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+          <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">About</a>
+          <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Stats</a>
+          <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Moves</a>
+        </div>
+      </nav>
+        <div class="tab-content" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <p>Height: ${heightDisplay}</p>
+            <p>Weight: ${weightDisplay}</p>
+          <div>
+            <p>Abilities</p>
+            <ul>
+              ${abilitiesHTML}
+            </ul>
+          </div>
+          </div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+          <ul class="list-group list-group-flush">
+        ${statsHTML}
+          </ul>
+          </div>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+        </div>
+
   <div class="card-body">
     <img class="arrow-btn" src="./img/icons8-links-eingekreist-50.png" alt="Pfeil links" onclick="changeDirection(-1)">
     <img class="arrow-btn" src="./img/icons8-pfeil-rechts-50.png" alt="Pfeil rechts" onclick="changeDirection(+1)">
@@ -145,3 +188,4 @@ function changeDirection(direction) {
 
   renderPokemoninOverlay(pokemon);
 }
+console.log(pokemonArr);
